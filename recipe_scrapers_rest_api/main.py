fcd4c2524f7a423ca9fb3da4c07cb5673c474d9b
Matthew The <matthew.the@tum.de>
2022-04-30 23:27:59 +0200

    First working prototype

---
+++ recipe_scrapers_rest_api/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from recipe_scrapers import scrape_me, WebsiteNotImplementedError
+
+
+app = Flask(__name__)
+
+
+@app.route('/recipes/')
+def recipes():
+    url = request.args.get('url', default = None, type = str)
+    
+    try:
+        scraper = scrape_me(url)
+        return jsonify({"title" : scraper.title(), "ingredients" : scraper.ingredients(), "servings" : scraper.yields().replace(" servings", "")})
+    except WebsiteNotImplementedError:
+        return {}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
