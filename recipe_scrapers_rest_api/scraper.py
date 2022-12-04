981532c83074376ba3ab6aef07775cbae474c266
Matthew The <matthew.the@tum.de>
2022-12-04 14:06:22 +0100

    Bump recipe scrapers to version 14.24.0

--- recipe_scrapers_rest_api/scraper.py
+++ recipe_scrapers_rest_api/scraper.py
@@ -1,6 +1,7 @@
 from recipe_scrapers import scrape_me, WebsiteNotImplementedError, NoSchemaFoundInWildMode
 import requests
 
+
 def scrape_recipe_url(url):    
     try:
         scraper = scrape_me(url)
@@ -8,10 +9,13 @@ def scrape_recipe_url(url):
         try:
             scraper = scrape_me(url, wild_mode=True)
         except NoSchemaFoundInWildMode:
+            print("No schema found in wild mode")
             return None
         except requests.exceptions.MissingSchema:
+            print("Missing schema in wild mode")
             return None
     except requests.exceptions.MissingSchema:
+        print("Missing schema")
         return None
     
     return scraper
